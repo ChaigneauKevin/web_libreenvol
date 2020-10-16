529af60d48673a48d397e06b48a41e605f7cde11
Chaigneau Kévin <71121297+ChaigneauKevin@users.noreply.github.com>
2020-10-16 13:30:10 +0200

    Menu changer

---
+++ vues/v_pied.php
@@ -0,0 +1,24 @@
+            <footer class="white-wrapper">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-md-6 text-left match-height">
+                            <ul class="list-inline social-links wow">
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="col-md-6 text-right match-height">
+                            <div class="vertical-center">
+                                <span class="copyright">Copyright 2019. LibreenvoL</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
